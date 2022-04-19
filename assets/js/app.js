e0082cdf1a5f1668e9ea6f30317ffab14ab92cb6
smradupan <martinelias630@gmail.com>
2022-04-19 10:31:14 +0200

    whole app.js code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,26 @@
 'use strict'
 
+const email = document.querySelector('#email');
+const form = document.querySelector('form');
+const error = document.querySelector('.error');
+
+function handleSubmit() {
+    event.preventDefault();
+
+    let regex = /^([a-z0-9]+(?:[._-][a-z0-9]+)*)@([a-z0-9]+(?:[.-][a-z0-9]+)*\.[a-z]{2,})$/i;
+
+    if(regex.test(email.value)) {
+
+        error.classList.add('hidden');
+        email.style.borderColor = '';
+        alert('Your email was sent succesfully!');
+
+    } else {
+
+        error.classList.remove('hidden');
+        email.style.borderColor = 'red';
+
+    }
+}
+
+form.addEventListener('submit', handleSubmit);
